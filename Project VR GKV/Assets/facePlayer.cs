a89f99a005abdd4677be8405e5168b42e489a9a4
hanya-manusia <syafiqrmuhammad@gmail.com>
2025-06-25 14:32:17 +0700

    avatar, map

---
+++ Project VR GKV/Assets/facePlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class facePlayer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = Camera.main.transform.position - transform.position;
+        if (lookDirection.sqrMagnitude > 0.01f)
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+    }
+}
---
+++ Project VR GKV/Assets/mapImage.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mapImage : MonoBehaviour
+{
+    public Sprite Map_zoomIn;
+    public Sprite Map_zoomOut;
+    public Sprite Sattelite_zoomIn;
+    public Sprite Sattelite_zoomOut;
+    public Button yourButton;
+
+    private Image img;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        yourButton.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        // Toggle between Map zoom in and out
+        if (img.sprite == Map_zoomIn)
+        {
+            img.sprite = Map_zoomOut;
+        }
+        else if (img.sprite == Map_zoomOut)
+        {
+            img.sprite = Map_zoomIn;
+        }
+        else if (img.sprite == Sattelite_zoomIn)
+        {
+            img.sprite = Sattelite_zoomOut;
+        }
+        else if (img.sprite == Sattelite_zoomOut)
+        {
+            img.sprite = Sattelite_zoomIn;
+        }
+    }
+
+    public void changeView()
+    {
+        if (img.sprite == Map_zoomIn)
+        {
+            img.sprite = Sattelite_zoomIn;
+        }
+        else if (img.sprite == Map_zoomOut)
+        {
+            img.sprite = Sattelite_zoomOut;
+        }
+        else if (img.sprite == Sattelite_zoomIn)
+        {
+            img.sprite = Map_zoomIn;
+        }
+        else if (img.sprite == Sattelite_zoomOut)
+        {
+            img.sprite = Map_zoomOut;
+        }
+    }
+}
